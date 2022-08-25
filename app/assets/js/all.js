71a65ec109d82a0aeeaa1397d21dcb19a61fa4de
YuJhen <j759153018@gmail.com>
2022-08-25 22:37:46 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,6 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+
